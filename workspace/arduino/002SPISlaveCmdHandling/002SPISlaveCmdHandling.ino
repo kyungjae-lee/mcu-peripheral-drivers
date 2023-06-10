d2cfe58eb0e496794556e8e33d02df08c130fdd1
Kyungjae Lee <kjxlee@gmail.com>
2023-06-10 15:23:01 -0500

    Update 'workspace/arduino/002SPISlaveCmdHandling/002SPISlaveCmdHandling.ino'

--- workspace/arduino/002SPISlaveCmdHandling/002SPISlaveCmdHandling.ino
+++ workspace/arduino/002SPISlaveCmdHandling/002SPISlaveCmdHandling.ino
@@ -1,10 +1,44 @@
+/*
+
+ * SPI pin numbers:
+ * SCK   13  // Serial Clock.
+ * MISO  12  // Master In Slave Out.
+ * MOSI  11  // Master Out Slave In.
+ * SS    10  // Slave Select
+ *
+
+ */
+
 #include <SPI.h>
 
-#define MAX_LEN 500
+const byte led = 9;           // Slave LED digital I/O pin.
 
-bool msgComplete = false;  // whether the string is complete
-uint8_t userBuffer[MAX_LEN];
-uint32_t cnt = 0;
+boolean ledState = HIGH;      // LED state flag.
+
+uint8_t dataBuff[255];
+
+uint8_t board_id[10] = "ARDUINOUNO";
+
+#define NACK 0xA5
+#define ACK 0xF5
+
+
+//command codes
+#define COMMAND_LED_CTRL          0x50
+#define COMMAND_SENSOR_READ       0x51
+#define COMMAND_LED_READ          0x52
+#define COMMAND_PRINT           0x53
+#define COMMAND_ID_READ         0x54
+
+#define LED_ON     1
+#define LED_OFF    0
+
+//arduino analog pins
+#define ANALOG_PIN0   0
+#define ANALOG_PIN1   1
+#define ANALOG_PIN2   2
+#define ANALOG_PIN3   3
+#define ANALOG_PIN4   4
 
 //Initialize SPI slave.
 void SPI_SlaveInit(void) 
@@ -14,6 +48,8 @@ void SPI_SlaveInit(void)
   pinMode(MOSI, INPUT);
   pinMode(MISO, OUTPUT);
   pinMode(SS, INPUT);
+  //make SPI as slave
+  
   // Enable SPI as slave.
   SPCR = (1 << SPE);
 }
@@ -37,63 +73,109 @@ void SPI_SlaveTransmit(uint8_t data)
   /* Wait for transmission complete */
   while(!(SPSR & (1<<SPIF)));
 }
- 
+  
+
+// The setup() function runs after reset.
 void setup() 
 {
   // Initialize serial for troubleshooting.
   Serial.begin(1200);
   
+  // Initialize slave LED pin.
+  pinMode(led, OUTPUT);
+  
+  digitalWrite(led,LOW);
+  
   // Initialize SPI Slave.
   SPI_SlaveInit();
-
-  pinMode(8, INPUT_PULLUP);
-  //digitalWrite(8,LOW);
-
+  
   Serial.println("Slave Initialized");
 }
 
-void notify_controller(void)
+
+byte checkData(byte commnad)
 {
-  pinMode(8,OUTPUT);
-  digitalWrite(8,HIGH);
-  delayMicroseconds(50);
-  digitalWrite(8,LOW);
+  //todo
+  return ACK;
 }
 
-
-void loop() {
+// The loop function runs continuously after setup().
+void loop() 
+{
+  byte data,command,len,ackornack=NACK;
   
-  Serial.println("Type anything and send...");
-
-  while(!msgComplete){
-    if (Serial.available()) {
-      //Read a byte of incoming serial data.
-      char readByte = (char)Serial.read();
-      //Accumalate in to the buffer
-      userBuffer[cnt++] = readByte;
-      if(readByte == '\r' || ( cnt == MAX_LEN)){
-        msgComplete = true;
-        userBuffer[cnt -1 ] = '\0'; //replace '\r' by '\0'
-      }
-    }
-  }
+  //1. fist make sure that ss is low . so lets wait until ss is low 
+  Serial.println("Slave waiting for ss to go low");
+  while(digitalRead(SS) );
   
-  Serial.println("Your message...");
-  Serial.println((char*)userBuffer);
-
+  //2. now lets wait until rx buffer has a byte
+  command = SPI_SlaveReceive();
+  ackornack = checkData(command);
   
-   notify_controller();
-
-  /*Transmit the user buffer over SPI */
-  for(uint32_t i = 0 ; i < cnt ; i++)
+  SPI_SlaveTransmit(ackornack);
+  
+  len = SPI_SlaveReceive(); //dummy byte
+  
+  if(command == COMMAND_LED_CTRL)
   {
-    SPI_SlaveTransmit(userBuffer[i]);
+    //read 2 more bytes pin number and value 
+    uint8_t pin = SPI_SlaveReceive(); 
+    uint8_t value = SPI_SlaveReceive(); 
+    Serial.println("RCVD:COMMAND_LED_CTRL");
+    if(value == (uint8_t)LED_ON)
+    {
+      digitalWrite(pin,HIGH);
+    }else if (value == (uint8_t) LED_OFF)
+    {
+      digitalWrite(pin,LOW);
+    }
+  
+  }else if ( command == COMMAND_SENSOR_READ)
+  {
+    //read analog pin number 
+    uint16_t aread;
+    uint8_t pin = SPI_SlaveReceive(); 
+    //pinMode(pin+14, INPUT_PULLUP);
+    uint8_t val;
+    aread = analogRead(pin+14);
+    val = map(aread, 0, 1023, 0, 255);
+    
+    SPI_SlaveTransmit(val);
+  
+    val = SPI_SlaveReceive(); //dummy read
+    
+    Serial.println("RCVD:COMMAND_SENSOR_READ");
+  
+    
+  
+  }else if ( command == COMMAND_LED_READ)
+  {
+    uint8_t pin = SPI_SlaveReceive(); 
+    uint8_t val = digitalRead(pin);
+    SPI_SlaveTransmit(val);
+    val = SPI_SlaveReceive(); //dummy read
+    Serial.println("RCVD:COMMAND_LED_READ");
+  
+  }else if ( command == COMMAND_PRINT)
+  {
+    uint8_t len = SPI_SlaveReceive(); 
+    for(int i=0 ; i < len ; i++)
+    {
+      dataBuff[i] = SPI_SlaveReceive();
+    }
+    Serial.println((char*)dataBuff);
+    
+    Serial.println("RCVD:COMMAND_PRINT");
+  
+  }else if ( command == COMMAND_ID_READ)
+  {
+    for(int i=0 ; i < strlen(board_id) ; i++)
+    {
+      SPI_SlaveTransmit(board_id[i]);
+    }
+      SPI_SlaveReceive();
+    Serial.println("RCVD:COMMAND_ID_READ");
   }
-  cnt = 0;
-  msgComplete = false;
-  Serial.println("Message sent...");
-
-  while(!digitalRead(SS));
-  Serial.println("Master ends communication");
  
+
 }
